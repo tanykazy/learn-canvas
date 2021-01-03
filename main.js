64f9e0868fae7f7f9467dbda65697e8b71712025
t-cool <hiroki802@hotmail.com>
2021-01-03 19:00:22 +0900

    常に三角形を表示させる

--- main.js
+++ main.js
@@ -90,6 +90,25 @@ myPics.addEventListener('mousedown',(e)=>{
 
 });
 
+setInterval(() => {
+
+  // もし 3 点が選択されたら三角形を描写する
+  if(selectedDots.size == 3){
+    let dots = Array.from(selectedDots.values());
+    context.beginPath();
+    context.moveTo(coordinates[dots[0]].x, coordinates[dots[0]].y); //最初の点の場所
+    context.lineTo(coordinates[dots[1]].x, coordinates[dots[1]].y); //2番目の点の場所
+    context.lineTo(coordinates[dots[2]].x, coordinates[dots[2]].y); //3番目の点の場所
+    context.closePath();	//三角形の最後の線 closeさせる
+    context.strokeStyle = "rgb(0,0,0)"; //枠線の色
+    context.stroke();
+    context.fillStyle="rgba(0,0,255,0.1)";//塗りつぶしの色
+    context.fill();
+  }
+
+  
+}, 500);
+
 myPics.addEventListener('mousemove', e => {
  
   // 座標と選択された dot を表示する
