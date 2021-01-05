407841e87edbd468cfc4f6581fb7e7bdc0117323
t-cool <hiroki802@hotmail.com>
2021-01-05 18:17:32 +0900

    色をつける機能を追加

--- main.js
+++ main.js
@@ -5,26 +5,27 @@ const mouseXY = document.getElementById('mouseXY');
 
 // 点1~12 の座標
 const coordinates = {
-  dot1:	{x: 100, y: 6},
-  dot2:	{x: 149, y: 18},
-  dot3:	{x: 183, y: 51},
-  dot4:	{x: 194, y: 100},
-  dot5:	{x: 182, y: 150},
-  dot6:	{x: 148, y: 181},
-  dot7:	{x: 100, y: 194},
-  dot8:	{x:  52, y: 182},
-  dot9:	{x:  18, y: 150},
-  dot10:{x:   6, y: 100},
-  dot11:{x:  18, y: 51},
-  dot12:{x:  53, y: 18}
+  dot1:	{x: 100, y:   4},
+  dot2:	{x: 148, y:  16.8615616},
+  dot3:	{x: 183.1384384, y:  52},
+  dot4:	{x: 196, y: 100},
+  dot5:	{x: 183.1384384, y: 148},
+  dot6:	{x: 148, y: 183.1384384},
+  dot7:	{x: 100, y: 196},
+  dot8:	{x:  52, y: 183.1384384},
+  dot9:	{x:  16.8615616, y: 148},
+  dot10:{x:  4, y: 100},
+  dot11:{x: 16.8615616, y:  52},
+  dot12:{x:  52, y:  16.8615616}
 }
 
 // 選択された dots
 const selectedDots = new Set();
 
 // 円を描く
+context.strokeStyle = "black"
 context.beginPath();
-context.arc(100, 100, 95, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+context.arc(100, 100, 96, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
 context.fillStyle = "rgba(0,0,0,0)";
 context.fill();
 context.stroke();
@@ -32,10 +33,41 @@ context.stroke();
 // 12点を描く
 for(let key in coordinates){
   context.beginPath();
-  context.arc(coordinates[key].x, coordinates[key].y, 5, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+  context.strokeStyle = "black"
+  context.arc(coordinates[key].x, coordinates[key].y, 3, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
   context.fillStyle = "rgba(0,0,0,0.7)";
   context.fill();
   context.stroke();
+}
+
+// 線の長さを元にラインの色を決定する
+function setLineColor (dot1,dot2) {
+  let distanceOfDots = 
+  Math.round(
+    Math.sqrt(
+      Math.pow((coordinates[dot1].x - coordinates[dot2].x),2) + 
+      Math.pow((coordinates[dot1].y - coordinates[dot2].y),2)))
+
+  console.log(distanceOfDots);
+
+  if(distanceOfDots == 50){
+    context.strokeStyle = "red";
+  }
+  else if(distanceOfDots == 96){
+    context.strokeStyle = "blue";
+  }
+  else if(distanceOfDots == 136){
+    context.strokeStyle = "yellow";
+  }
+  else if(distanceOfDots == 166){
+    context.strokeStyle = "green";
+  }
+  else if(distanceOfDots == 185){
+    context.strokeStyle = "orange";
+  }
+  else if(distanceOfDots == 192){
+    context.strokeStyle = "pink";
+  }
 }
 
 // 12点周辺でのクリック時の処理
@@ -46,17 +78,13 @@ myPics.addEventListener('mousedown',(e)=>{
     if((e.offsetX > coordinates[key].x - 15 && e.offsetX < coordinates[key].x + 15) 
         && (e.offsetY > coordinates[key].y - 15 && e.offsetY < coordinates[key].y + 15)){
       context.beginPath();
-      context.arc(coordinates[key].x, coordinates[key].y, 5, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+      context.arc(coordinates[key].x, coordinates[key].y, 4, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
       context.fillStyle = "rgba(255,200,0,0.8)";
       context.fill();
       context.stroke();
       selectedDots.add(key);
     }
   }
-});
-
-// 0.5秒ごとにdotの選択状況を確認する
-setInterval(() => {
 
   // 4点目が選択されたときの処理
   if(selectedDots.size > 3){
@@ -69,13 +97,14 @@ setInterval(() => {
     context.beginPath();
     context.arc(100, 100, 95, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
     context.fillStyle = "rgba(0,0,0,0)";
+    context.strokeStyle = "black"
     context.fill();
     context.stroke();
 
     // 12点を描く
     for(let key in coordinates){
       context.beginPath();
-      context.arc(coordinates[key].x, coordinates[key].y, 5, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
+      context.arc(coordinates[key].x, coordinates[key].y, 4, 0 * Math.PI / 180, 360 * Math.PI / 180, false );
       context.fillStyle = "rgba(0,0,0,0.7)";
       context.fill();
       context.stroke();
@@ -85,27 +114,37 @@ setInterval(() => {
   // 2点が選択されたら線を引く
   if(selectedDots.size == 2){
     let dots = Array.from(selectedDots.values());
+
     context.beginPath();
+    setLineColor(dots[0],dots[1]);
+    console.log(dots[0],dots[1])
     context.moveTo(coordinates[dots[0]].x, coordinates[dots[0]].y); //最初の点の場所
     context.lineTo(coordinates[dots[1]].x, coordinates[dots[1]].y); //2番目の点の場所
-    context.closePath();	//三角形の最後の線 closeさせる
     context.stroke();
   }
 
   // 3点が選択されたら三角形を描写する
   if(selectedDots.size == 3){
     let dots = Array.from(selectedDots.values());
+
+    // 2点目から3点目
     context.beginPath();
-    context.moveTo(coordinates[dots[0]].x, coordinates[dots[0]].y); //最初の点の場所
-    context.lineTo(coordinates[dots[1]].x, coordinates[dots[1]].y); //2番目の点の場所
-    context.lineTo(coordinates[dots[2]].x, coordinates[dots[2]].y); //3番目の点の場所
-    context.closePath();	//三角形の最後の線 closeさせる
-    context.strokeStyle = "rgb(0,0,0)"; //枠線の色
+    setLineColor(dots[1],dots[2]);
+    console.log(dots[1],dots[2])
+    context.moveTo(coordinates[dots[1]].x, coordinates[dots[1]].y);
+    context.lineTo(coordinates[dots[2]].x, coordinates[dots[2]].y);
     context.stroke();
-    context.fillStyle="rgba(0,0,255,0.1)";//塗りつぶしの色
-    context.fill();
+
+    // 3点目から1点目
+    setLineColor(dots[2],dots[0]);
+    console.log(dots[2],dots[0])
+    context.moveTo(coordinates[dots[2]].x, coordinates[dots[2]].y);
+    context.lineTo(coordinates[dots[0]].x, coordinates[dots[0]].y);
+    context.stroke();
+    context.closePath();	//三角形の最後の線 closeさせる
   }
-}, 500);
+
+});
 
 // マウスを動かしたときの処理
 /*
